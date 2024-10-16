0b071a88cbeab24e39199a36511f7f0f067b0e7a
MikiP98 <pokora.mikolaj@gmail.com>
2024-10-16 20:27:12 +0200

    Sack update

--- src/main/java/io/github/mikip98/savethehotbar/blocks/Sack.java
+++ src/main/java/io/github/mikip98/savethehotbar/blocks/Sack.java
@@ -1,9 +1,21 @@
 package io.github.mikip98.savethehotbar.blocks;
 
 import net.minecraft.block.Block;
+import net.minecraft.block.BlockEntityProvider;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Waterloggable;
+import net.minecraft.block.entity.BlockEntity;
+import net.minecraft.util.math.BlockPos;
+import org.jetbrains.annotations.Nullable;
 
-public class Sack extends Block {
+public class Sack extends Block implements BlockEntityProvider, Waterloggable {
     public Sack(Settings settings) {
         super(settings);
+    }
+
+    @Nullable
+    @Override
+    public BlockEntity createBlockEntity(BlockPos pos, BlockState state) {
+        return null;
     }
 }
