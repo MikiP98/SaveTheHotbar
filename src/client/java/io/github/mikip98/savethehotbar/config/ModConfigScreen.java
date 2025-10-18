f4ccfbd3410e47e645a6dc440dc1b821a5caa118
MikiP98 <pokora.mikolaj@gmail.com>
2025-10-18 23:06:44 +0200

    Improved in game Config Screen and fixed some missing/duplicated settings

--- src/client/java/io/github/mikip98/savethehotbar/config/ModConfigScreen.java
+++ src/client/java/io/github/mikip98/savethehotbar/config/ModConfigScreen.java
@@ -7,6 +7,7 @@ import io.github.mikip98.savethehotbar.config.io.ConfigSaver;
 import me.shedaniel.clothconfig2.api.ConfigBuilder;
 import me.shedaniel.clothconfig2.api.ConfigCategory;
 import me.shedaniel.clothconfig2.api.ConfigEntryBuilder;
+import me.shedaniel.clothconfig2.impl.builders.SubCategoryBuilder;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.text.Text;
 
@@ -15,19 +16,22 @@ public class ModConfigScreen {
         ConfigBuilder builder = ConfigBuilder.create()
                 .setSavingRunnable(ConfigSaver::saveConfigToFile)
                 .setParentScreen(parentScreen)
-                .setTitle(Text.literal("SaveTheHotbar! Configuration Screen"));
+                .setTitle(Text.of("SaveTheHotbar! Configuration Screen"));
 
         // Create a root category
         ConfigCategory rootCategory = builder.getOrCreateCategory(Text.literal("General Settings"));
 
+        // --- Main toggle ---
         rootCategory.addEntry(ConfigEntryBuilder.create()
                 .startBooleanToggle(Text.of("Enabled"), ModConfig.enabled)
-                .setTooltip(Text.of("If the mod should be enabled"))
+                .setTooltip(Text.of("Should the mod be enabled"))
                 .setDefaultValue(ModConfig.dEnabled)
                 .setSaveConsumer(value -> ModConfig.enabled = value)
                 .build()
         );
 
+        // --- Item Slots ---
+        // Vanilla
         rootCategory.addEntry(ConfigEntryBuilder.create()
                 .startBooleanToggle(Text.literal("Save Hotbar"), ModConfig.saveHotbar)
                 .setTooltip(Text.literal("Keep the all the hotbar items after death"))
@@ -49,7 +53,18 @@ public class ModConfigScreen {
                 .setSaveConsumer(value -> ModConfig.saveSecondHand = value)
                 .build()
         );
+        // Modded
+        SubCategoryBuilder moddedSlotSettings = ConfigEntryBuilder.create().startSubCategory(Text.of("Modded Slot Settings"));
+        moddedSlotSettings.add(ConfigEntryBuilder.create()
+                .startBooleanToggle(Text.of("Save Arsenal Back Slot"), ModConfig.saveArsenal)
+                .setTooltip(Text.of("Keep Arsenal's back slot after death"))
+                .setDefaultValue(ModConfig.dSaveArsenal)
+                .setSaveConsumer(value -> ModConfig.saveArsenal = value)
+                .build()
+        );
+        rootCategory.addEntry(moddedSlotSettings.build());
 
+        // --- EXP ---
         rootCategory.addEntry(ConfigEntryBuilder.create()
                 .startEnumSelector(Text.of("Experience Behaviour"), ExperienceMode.class, ModConfig.experienceBehaviour)
                 .setTooltip(Text.of("""
@@ -71,8 +86,7 @@ public class ModConfigScreen {
                         - FRACTION -> The experience is multiplied by Experience Fraction
                         - VANILLA -> New experience will be equal to '{player_level} * 7'
                         The modes are sorted from the most to the least forgiving
-                        ALL wil result in the most EXP, VANILLA with the least*
-                        *FRACTION mode depends on the 'Experience Fraction' setting, if set very low it can result in less exp than VANILLA
+                        ALL wil result in the most EXP, VANILLA with the least (unless 'Experience Fraction' is set very low)
                         """))
                 .setDefaultValue(ModConfig.dExperienceCalculationMode)
                 .setSaveConsumer(value -> ModConfig.experienceCalculationMode = value)
@@ -89,27 +103,42 @@ public class ModConfigScreen {
         );
 
 
+        SubCategoryBuilder randomItemLoseChanceSettings = ConfigEntryBuilder.create().startSubCategory(Text.of("Random Item Lose Chance Settings"));
+        randomItemLoseChanceSettings.add(ConfigEntryBuilder.create()
+                .startFloatField(Text.literal("Random Drop Chance"), ModConfig.randomDropChance)
+                .setTooltip(Text.literal("The chance that a normally kept item will be randomly dropped/stored (e.g. a hotbar item) (0.0 -> 0%; 1.0 -> 100%)"))
+                .setDefaultValue(DefaultConfig.dRandomDropChance)
+                .setMax(1.0f)
+                .setMin(0.0f)
+                .setSaveConsumer(value -> ModConfig.randomDropChance = value)
+                .build()
+        );
+        randomItemLoseChanceSettings.add(ConfigEntryBuilder.create()
+                .startFloatField(Text.literal("Rarity Drop Chance Decrease"), ModConfig.rarityDropChanceDecrease)
+                .setTooltip(Text.literal("A percentage by which the random drop chance will be decreased (e.g. rdc = 20%, rdcd = 20%, UNCOMMON item will have 16% change to drop)"))
+                .setDefaultValue(DefaultConfig.dRarityDropChanceDecrease)
+                .setMax(1.0f)
+                .setMin(0.0f)
+                .setSaveConsumer(value -> ModConfig.rarityDropChanceDecrease = value)
+                .build()
+        );
+        randomItemLoseChanceSettings.add(ConfigEntryBuilder.create()
+                .startFloatField(Text.literal("Luck Drop Chance Decrease"), ModConfig.luckDropChanceDecrease)
+                .setTooltip(Text.literal("A percentage by which the random drop chance will be decreased (e.g. rdc = 20%, ldcd = 20%, with luck 1, item will have 16% change to drop)"))
+                .setDefaultValue(DefaultConfig.dRarityDropChanceDecrease)
+                .setMax(1.0f)
+                .setMin(0.0f)
+                .setSaveConsumer(value -> ModConfig.luckDropChanceDecrease = value)
+                .build()
+        );
+        rootCategory.addEntry(randomItemLoseChanceSettings.build());
+
+
         rootCategory.addEntry(ConfigEntryBuilder.create()
                 .startBooleanToggle(Text.literal("Random Item Spread"), ModConfig.randomSpread)
-                .setTooltip(Text.literal("If `False`, all the items will drop in the exact position of your death and will not spread outwards. If `True`, all the dropped items will be spread randomly in all directions"))
+                .setTooltip(Text.literal("If `False`, all the items will drop in the exact position of your death and will not spread outwards.\nIf `True`, all the dropped items will be spread randomly in all directions like in vanilla"))
                 .setDefaultValue(DefaultConfig.dRandomSpread)
                 .setSaveConsumer(value -> ModConfig.randomSpread = value)
-                .build()
-        );
-
-        rootCategory.addEntry(ConfigEntryBuilder.create()
-                .startBooleanToggle(Text.literal("Log Death Coordinates in Chat"), ModConfig.logDeathCoordinatesInChat)
-                .setTooltip(Text.literal("If `True`, the death coordinates will be sent in chat upon player death"))
-                .setDefaultValue(DefaultConfig.dLogDeathCoordinatesInChat)
-                .setSaveConsumer(value -> ModConfig.logDeathCoordinatesInChat = value)
-                .build()
-        );
-
-        rootCategory.addEntry(ConfigEntryBuilder.create()
-                .startBooleanToggle(Text.literal("Log Grave Coordinates in Chat"), ModConfig.logGraveCoordinatesInChat)
-                .setTooltip(Text.literal("If `True`, the grave spawn coordinates will be sent in chat upon player death"))
-                .setDefaultValue(DefaultConfig.dLogGraveCoordinatesInChat)
-                .setSaveConsumer(value -> ModConfig.logGraveCoordinatesInChat = value)
                 .build()
         );
 
@@ -143,69 +172,49 @@ public class ModConfigScreen {
                 .build()
         );
 
-        rootCategory.addEntry(ConfigEntryBuilder.create()
-                .startFloatField(Text.literal("Random Drop Chance"), ModConfig.randomDropChance)
-                .setTooltip(Text.literal("The chance that a normally kept item will be randomly dropped (e.g. a hotbar item) (0.0 -> 0%; 1.0 -> 100%)"))
-                .setDefaultValue(DefaultConfig.dRandomDropChance)
-                .setMax(1.0f)
-                .setMin(0.0f)
-                .setSaveConsumer(value -> ModConfig.randomDropChance = value)
+        SubCategoryBuilder graveSpawningLogic = ConfigEntryBuilder.create().startSubCategory(Text.of("Grave Spawning Logic"));
+        graveSpawningLogic.add(ConfigEntryBuilder.create()
+                .startIntField(Text.literal("Sack Max Spawn Radius"), ModConfig.sackMaxSpawnRadius)
+                .setTooltip(Text.literal("The maximum search radius for a valid sack spawning position. If no valid place is found, a Sack will be spawned directly at the death position replacing whatever block was there."))
+                .setDefaultValue(DefaultConfig.dSackMaxSpawnRadius)
+                .setMin(0)
+                .setSaveConsumer(value -> ModConfig.sackMaxSpawnRadius = value)
                 .build()
         );
-
-        rootCategory.addEntry(ConfigEntryBuilder.create()
-                .startFloatField(Text.literal("Rarity Drop Chance Decrease"), ModConfig.rarityDropChanceDecrease)
-                .setTooltip(Text.literal("A percentage by which the random drop chance will be decreased (e.g. rdc = 20%, rdcd = 20%, UNCOMMON item will have 16% change to drop)"))
-                .setDefaultValue(DefaultConfig.dRarityDropChanceDecrease)
-                .setMax(1.0f)
-                .setMin(0.0f)
-                .setSaveConsumer(value -> ModConfig.rarityDropChanceDecrease = value)
-                .build()
-        );
-        rootCategory.addEntry(ConfigEntryBuilder.create()
-                .startFloatField(Text.literal("Luck Drop Chance Decrease"), ModConfig.luckDropChanceDecrease)
-                .setTooltip(Text.literal("A percentage by which the random drop chance will be decreased (e.g. rdc = 20%, ldcd = 20%, with luck 1, item will have 16% change to drop)"))
-                .setDefaultValue(DefaultConfig.dRarityDropChanceDecrease)
-                .setMax(1.0f)
-                .setMin(0.0f)
-                .setSaveConsumer(value -> ModConfig.luckDropChanceDecrease = value)
-                .build()
-        );
-
-        rootCategory.addEntry(ConfigEntryBuilder.create()
-                .startIntField(Text.literal("Mob Grave Max Spawn Radius"), ModConfig.mobGraveMaxSpawnRadius)
+        graveSpawningLogic.add(ConfigEntryBuilder.create()
+                .startIntField(Text.literal("Mob Head Grave Max Spawn Radius"), ModConfig.mobGraveMaxSpawnRadius)
                 .setTooltip(Text.literal("The maximum search radius for a valid mob grave spawning position. If no valid place will be found, a Sack will be spawned instead"))
                 .setDefaultValue(DefaultConfig.dMobGraveMaxSpawnRadius)
                 .setMin(0)
                 .setSaveConsumer(value -> ModConfig.mobGraveMaxSpawnRadius = value)
                 .build()
         );
-        rootCategory.addEntry(ConfigEntryBuilder.create()
-                .startIntField(Text.literal("Mob Grave Max Spawn Radius"), ModConfig.mobGraveMaxSpawnRadius)
-                .setTooltip(Text.literal("The maximum search radius for a valid mob grave spawning position. If no valid place will be found, a Sack will be spawned instead"))
-                .setDefaultValue(DefaultConfig.dMobGraveMaxSpawnRadius)
-                .setMin(0)
-                .setSaveConsumer(value -> ModConfig.mobGraveMaxSpawnRadius = value)
-                .build()
-        ); // TODO
-        rootCategory.addEntry(ConfigEntryBuilder.create()
-                .startIntField(Text.literal("Mob Grave Max Spawn Radius"), ModConfig.mobGraveMaxSpawnRadius)
-                .setTooltip(Text.literal("The maximum search radius for a valid mob grave spawning position. If no valid place will be found, a Sack will be spawned instead"))
-                .setDefaultValue(DefaultConfig.dMobGraveMaxSpawnRadius)
-                .setMin(0)
-                .setSaveConsumer(value -> ModConfig.mobGraveMaxSpawnRadius = value)
-                .build()
-        ); // TODO
-
-        ConfigCategory modSupportCategory = builder.getOrCreateCategory(Text.literal("Mod Support Settings"));
-
-        modSupportCategory.addEntry(ConfigEntryBuilder.create()
-                .startBooleanToggle(Text.of("Save Arsenal Back Slot"), ModConfig.saveArsenal)
-                .setTooltip(Text.of("Keep Arsenal's back slot after death"))
-                .setDefaultValue(ModConfig.dSaveArsenal)
-                .setSaveConsumer(value -> ModConfig.saveArsenal = value)
+        graveSpawningLogic.add(ConfigEntryBuilder.create()
+                .startBooleanToggle(Text.of("Allow Mob Head Graves To Spawn On Slabs"), ModConfig.allowGravesToSpawnOnSlabs)
+                .setTooltip(Text.of("If `True`, a top slab block will also be considered a valid grave spawning block"))
+                .setDefaultValue(DefaultConfig.dAllowGravesToSpawnOnSlabs)
+                .setSaveConsumer(value -> ModConfig.allowGravesToSpawnOnSlabs = value)
                 .build()
         );
+        rootCategory.addEntry(graveSpawningLogic.build());
+
+
+        rootCategory.addEntry(ConfigEntryBuilder.create()
+                .startBooleanToggle(Text.literal("Log Death Coordinates in Chat"), ModConfig.logDeathCoordinatesInChat)
+                .setTooltip(Text.literal("If `True`, the death coordinates will be sent in chat upon player death"))
+                .setDefaultValue(DefaultConfig.dLogDeathCoordinatesInChat)
+                .setSaveConsumer(value -> ModConfig.logDeathCoordinatesInChat = value)
+                .build()
+        );
+
+        rootCategory.addEntry(ConfigEntryBuilder.create()
+                .startBooleanToggle(Text.literal("Log Grave Coordinates in Chat"), ModConfig.logGraveCoordinatesInChat)
+                .setTooltip(Text.literal("If `True`, the grave spawn coordinates will be sent in chat upon player death"))
+                .setDefaultValue(DefaultConfig.dLogGraveCoordinatesInChat)
+                .setSaveConsumer(value -> ModConfig.logGraveCoordinatesInChat = value)
+                .build()
+        );
+
 
         return builder.build();
     }
