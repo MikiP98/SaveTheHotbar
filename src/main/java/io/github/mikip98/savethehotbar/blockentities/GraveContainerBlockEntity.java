f347f470b8229273bf19774f5af36259945a7b14
MikiP98 <pokora.mikolaj@gmail.com>
2025-10-18 12:39:49 +0200

    Added support for the graves to store EXP and modded items

--- src/main/java/io/github/mikip98/savethehotbar/blockentities/GraveContainerBlockEntity.java
+++ src/main/java/io/github/mikip98/savethehotbar/blockentities/GraveContainerBlockEntity.java
@@ -1,13 +1,14 @@
 package io.github.mikip98.savethehotbar.blockentities;
 
 import io.github.mikip98.savethehotbar.SaveTheHotbar;
+import io.github.mikip98.savethehotbar.modDetection.SupportedSlotMods;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.entity.BlockEntity;
-import net.minecraft.block.entity.BlockEntityType;
 import net.minecraft.inventory.Inventories;
 import net.minecraft.inventory.SidedInventory;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NbtCompound;
+import net.minecraft.nbt.NbtList;
 import net.minecraft.network.listener.ClientPlayPacketListener;
 import net.minecraft.network.packet.Packet;
 import net.minecraft.network.packet.s2c.play.BlockEntityUpdateS2CPacket;
@@ -16,36 +17,123 @@ import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
 import org.jetbrains.annotations.Nullable;
 
-import java.util.ArrayList;
+import java.util.EnumMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Stream;
+
+import static io.github.mikip98.savethehotbar.SaveTheHotbar.LOGGER;
 
 public class GraveContainerBlockEntity extends BlockEntity implements GraveContainerInventory, SidedInventory {
-    private final DefaultedList<ItemStack> items = DefaultedList.ofSize(41, ItemStack.EMPTY);
+    protected final DefaultedList<ItemStack> items = DefaultedList.ofSize(41, ItemStack.EMPTY);
+    protected final EnumMap<SupportedSlotMods, DefaultedList<ItemStack>> moddedItems = createEnumMap();
+    protected int exp = 0;
 
-    public GraveContainerBlockEntity(BlockEntityType<?> type, BlockPos pos, BlockState state) { super(type, pos, state); }
-    public GraveContainerBlockEntity(BlockPos pos, BlockState state) { super(SaveTheHotbar.GRAVE_CONTAINER_BLOCK_ENTITY, pos, state); }
-
-
-    @Override
-    public DefaultedList<ItemStack> getItems() { return this.items; }
-
-    public void setItems(ArrayList<ItemStack> items) {
-        this.items.clear();
-        for (int i = 0; i < items.size(); i++) {
-            this.items.set(i, items.get(i));
+    protected EnumMap<SupportedSlotMods, DefaultedList<ItemStack>> createEnumMap() {
+        EnumMap<SupportedSlotMods, DefaultedList<ItemStack>> moddedItems = new EnumMap<>(SupportedSlotMods.class);
+        for (SupportedSlotMods mod : SupportedSlotMods.values()) {
+            if (mod.isLoaded()) moddedItems.put(mod, DefaultedList.ofSize(mod.slotAmount, ItemStack.EMPTY));
         }
+        return moddedItems;
     }
 
+    public GraveContainerBlockEntity(BlockPos pos, BlockState state) {
+        super(SaveTheHotbar.GRAVE_CONTAINER_BLOCK_ENTITY, pos, state);
+    }
+
+    @Override
+    public DefaultedList<ItemStack> getItems() {
+        final List<ItemStack> allItems = Stream.concat(this.items.stream(), this.moddedItems.values().stream().flatMap(List::stream)).toList();
+        DefaultedList<ItemStack> defaultedList = DefaultedList.ofSize(allItems.size(), ItemStack.EMPTY);
+        for (int i = 0; i < allItems.size(); i++) { defaultedList.set(i, allItems.get(i)); }
+        return defaultedList;
+    }
+    public int getExp() {
+        return exp;
+    }
+
+    public void setItems(List<ItemStack> vanillaItems, Map<SupportedSlotMods, List<ItemStack>> moddedItems) {
+        this.items.clear();
+        for (int i = 0; i < vanillaItems.size(); i++) {
+            this.items.set(i, vanillaItems.get(i));
+        }
+        for (SupportedSlotMods mod : SupportedSlotMods.values()) {
+            if (mod.isLoaded()) {
+                final List<ItemStack> items = moddedItems.get(mod);
+                DefaultedList<ItemStack> defaultedList = DefaultedList.ofSize(items.size(), ItemStack.EMPTY);
+                for (int i = 0; i < items.size(); i++) {
+                    defaultedList.set(i, items.get(i));
+                }
+                this.moddedItems.put(mod, defaultedList);
+            }
+        }
+    }
+    public void setExp(int exp) {
+        this.exp = exp;
+    }
 
     @Override
     public void readNbt(NbtCompound nbt) {
         super.readNbt(nbt);
         Inventories.readNbt(nbt, items);
+        for (Map.Entry<SupportedSlotMods, DefaultedList<ItemStack>> entry : moddedItems.entrySet()) {
+            tryReadModdedItemNbt(nbt, entry.getValue(), entry.getKey());
+        }
+        this.exp = nbt.getInt("Experience");
+    }
+
+    /**
+     * Code taken from 'Inventories.readNBT()'
+     * Located in package 'net.minecraft.inventory'
+     * Modified to accept a mod
+     */
+    public static void tryReadModdedItemNbt(NbtCompound nbt, DefaultedList<ItemStack> stacks, SupportedSlotMods mod) {
+        if (!mod.isLoaded()) return;
+
+        final String nbtId = "Items" + mod.modName;
+        if (!nbt.contains(nbtId)) return;
+
+        NbtList nbtList = nbt.getList(nbtId, 10);
+
+        for (int i = 0; i < nbtList.size(); i++) {
+            NbtCompound nbtCompound = nbtList.getCompound(i);
+            int j = nbtCompound.getByte("Slot") & 255;
+            if (j < stacks.size()) {
+                stacks.set(j, ItemStack.fromNbt(nbtCompound));
+            }
+        }
     }
 
     @Override
     public void writeNbt(NbtCompound nbt) {
         Inventories.writeNbt(nbt, items);
+        for (Map.Entry<SupportedSlotMods, DefaultedList<ItemStack>> entry : moddedItems.entrySet()) {
+            writeNbt(nbt, entry.getValue(), entry.getKey());
+        }
+        nbt.putInt("Experience", this.exp);
         super.writeNbt(nbt);
+    }
+
+    /**
+     * Code taken from 'Inventories.writeNbt()'
+     * Located in package 'net.minecraft.inventory'
+     * Modified to accept a mod
+     */
+    public static void writeNbt(NbtCompound nbt, DefaultedList<ItemStack> stacks, SupportedSlotMods mod) {
+        if (!mod.isLoaded() || stacks.isEmpty() || stacks.stream().allMatch((stack) -> stack == ItemStack.EMPTY)) return;
+        NbtList nbtList = new NbtList();
+
+        for (int i = 0; i < stacks.size(); i++) {
+            ItemStack itemStack = stacks.get(i);
+            if (!itemStack.isEmpty()) {
+                NbtCompound nbtCompound = new NbtCompound();
+                nbtCompound.putByte("Slot", (byte) i);
+                itemStack.writeNbt(nbtCompound);
+                nbtList.add(nbtCompound);
+            }
+        }
+
+        nbt.put("Items" + mod.modName, nbtList);
     }
 
     @Nullable
--- src/main/java/io/github/mikip98/savethehotbar/blocks/GraveContainer.java
+++ src/main/java/io/github/mikip98/savethehotbar/blocks/GraveContainer.java
@@ -1,6 +1,7 @@
 package io.github.mikip98.savethehotbar.blocks;
 
 import io.github.mikip98.savethehotbar.blockentities.GraveContainerBlockEntity;
+import io.github.mikip98.savethehotbar.deathProcessing.DeathManager;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockEntityProvider;
 import net.minecraft.block.BlockState;
@@ -12,9 +13,11 @@ import net.minecraft.item.ItemPlacementContext;
 import net.minecraft.item.ItemStack;
 import net.minecraft.state.StateManager;
 import net.minecraft.state.property.Properties;
-import net.minecraft.util.collection.DefaultedList;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
+import org.jetbrains.annotations.NotNull;
+
+import static io.github.mikip98.savethehotbar.SaveTheHotbar.LOGGER;
 
 public class GraveContainer extends Block implements BlockEntityProvider, Waterloggable {
     @Override
@@ -24,37 +27,49 @@ public class GraveContainer extends Block implements BlockEntityProvider, Waterl
 
     public GraveContainer(Settings settings) {
         super(settings);
-        setDefaultState(getStateManager().getDefaultState().with(Properties.WATERLOGGED, false));
+        setDefaultState(getDefaultState()
+                .with(Properties.WATERLOGGED, false)
+        );
     }
 
     @Override
-    public BlockState getPlacementState(ItemPlacementContext ctx) {
-        return this.getDefaultState().with(Properties.WATERLOGGED, ctx.getWorld().getFluidState(ctx.getBlockPos()).getFluid() == Fluids.WATER);
+    public @NotNull BlockState getPlacementState(ItemPlacementContext ctx) {
+        return getDefaultState().with(
+                Properties.WATERLOGGED, ctx.getWorld().getFluidState(ctx.getBlockPos()).getFluid() == Fluids.WATER
+        );
     }
 
+    @SuppressWarnings("deprecation")
     @Override
     public FluidState getFluidState(BlockState state) {
         return state.get(Properties.WATERLOGGED) ? Fluids.WATER.getStill(false) : Fluids.EMPTY.getDefaultState();
     }
 
+    @SuppressWarnings("deprecation")
     @Override
     public void onStateReplaced(BlockState state, World world, BlockPos pos, BlockState newState, boolean moved) {
+        LOGGER.info("Grave state change detected!");
         if (state.getBlock() != newState.getBlock()) {
+            LOGGER.info("Grave has been destroyed!");
             BlockEntity blockEntity = world.getBlockEntity(pos);
-            if (blockEntity instanceof GraveContainerBlockEntity cabinetEntity) {
-                // Handle item drops and block entity cleanup here
-                DefaultedList<ItemStack> inventory = cabinetEntity.getItems();
-                for (ItemStack stack : inventory) {
-                    if (!stack.isEmpty()) {
-                        Block.dropStack(world, pos, stack);
-                    }
+            if (blockEntity instanceof GraveContainerBlockEntity graveContainerBlockEntity) {
+                LOGGER.info("Dropping '{}' items", graveContainerBlockEntity.itemCount());
+                for (ItemStack stack : graveContainerBlockEntity.getItems()) {
+                    if (!stack.isEmpty()) Block.dropStack(world, pos, stack);
+                }
+                final int exp = graveContainerBlockEntity.getExp();
+                LOGGER.info("Dropping '{}' exp", exp);
+                if (exp > 0) {
+                    DeathManager.dropEXP(exp, world, world.getRandom(), pos);
                 }
             }
-            super.onStateReplaced(state, world, pos, newState, moved);
         }
+        super.onStateReplaced(state, world, pos, newState, moved);
     }
 
 
     @Override
-    public BlockEntity createBlockEntity(BlockPos pos, BlockState state) { return new GraveContainerBlockEntity(pos, state); }
+    public BlockEntity createBlockEntity(BlockPos pos, BlockState state) {
+        return new GraveContainerBlockEntity(pos, state);
+    }
 }
