4ec74095f12c47c1494db6a543b00a13f613967a
MikiP98 <pokora.mikolaj@gmail.com>
2025-11-06 19:03:52 +0100

    Added item tags for the new item type filtering

---
+++ src/main/java/io/github/mikip98/savethehotbar/content/tags/ModItemTags.java
@@ -0,0 +1,23 @@
+package io.github.mikip98.savethehotbar.content.tags;
+
+import io.github.mikip98.savethehotbar.config.enums.itemTypes.VanillaItemTypes;
+import net.minecraft.item.Item;
+import net.minecraft.registry.RegistryKeys;
+import net.minecraft.registry.tag.TagKey;
+
+import java.util.EnumMap;
+import java.util.Map;
+
+import static io.github.mikip98.savethehotbar.SaveTheHotbar.getId;
+
+public class ModItemTags {
+    public static Map<VanillaItemTypes, TagKey<Item>> vanillaItemTypesTagOverridesMap = new EnumMap<>(VanillaItemTypes.class);
+    static {
+        for (VanillaItemTypes type : VanillaItemTypes.values()) {
+            if (type == VanillaItemTypes.OTHER) continue;
+            vanillaItemTypesTagOverridesMap.put(type,
+                    TagKey.of(RegistryKeys.ITEM, getId("material_type_" + type.name().toLowerCase() + "_override"))
+            );
+        }
+    }
+}
