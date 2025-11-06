bd28363642e74b17682b8c2cb73e055ea172219a
MikiP98 <pokora.mikolaj@gmail.com>
2025-11-06 18:56:10 +0100

    Added new VanillaItemTypes enum for filtering kept items by item types

---
+++ src/main/java/io/github/mikip98/savethehotbar/config/enums/itemTypes/VanillaItemTypes.java
@@ -0,0 +1,18 @@
+package io.github.mikip98.savethehotbar.config.enums.itemTypes;
+
+public enum VanillaItemTypes {
+    TOOL,
+    WEAPON,
+    AMMUNITION,
+
+    ARMOUR,
+    EQUIPMENT,
+
+    FOOD,
+    POTION,
+
+    LIGHT_SOURCE_ON,
+    POSSIBLE_LIGHT_SOURCE,
+
+    OTHER
+}
