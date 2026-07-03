20c6ffec0e640b4dff27ba9a2435917b9a0b5ef5
MikiP98 <pokora.mikolaj@gmail.com>
2026-07-03 21:52:46 +0200

    Added class for future EXP tests

---
+++ src/gametest/java/io/github/mikip98/savethehotbar/EXPTests.java
@@ -0,0 +1,7 @@
+package io.github.mikip98.savethehotbar;
+
+#if MC_VERSION < 12108  import net.fabricmc.fabric.api.gametest.v1.FabricGameTest; #endif
+
+public class EXPTests #if MC_VERSION < 12108 implements FabricGameTest #endif {
+
+}
